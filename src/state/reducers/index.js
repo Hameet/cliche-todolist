bdda8c8d1faa290f7997945d45cd354f6498b3fc
Hameet <hameet.singh@hotmail.com>
2018-11-01 21:27:56 +1300

    Delete function working

--- src/state/reducers/index.js
+++ src/state/reducers/index.js
@@ -9,16 +9,17 @@ function rootReducer (state = initialState, { payload = {}, type }) {
         ...state,
         todos: [...state.todos, payload]
       }
-    default:
-      return state
+
     case DELETE_TODO:
       return {
         ...state,
-        users: [
+        todos: [
           ...slice(0, payload.index, state.todos),
           ...slice(payload.index + 1, length(state.todos), state.todos)
         ]
       }
+    default:
+      return state
   }
 }
 
