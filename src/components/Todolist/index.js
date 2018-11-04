94ca49f6f3decc44d47305085c9b626b94ec6646
Hameet <hameet.singh@hotmail.com>
2018-11-04 14:37:38 +1300

    Added Styled Components

--- src/components/Todolist/index.js
+++ src/components/Todolist/index.js
@@ -1,8 +1,27 @@
 import React from 'react'
 import { addIndex, map } from 'ramda'
 import { isNilOrEmpty } from 'ramda-adjunct'
+import styled from 'styled-components'
 
 const indexedMap = addIndex(map)
+
+const Button = styled.button`
+  
+  background: ${props => (props.primary ? 'palevioletred' : 'white')};
+  color: ${props => (props.primary ? 'white' : 'palevioletred')};
+
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+`
+
+const Li = styled.li`
+  font-size: 2em;
+  padding: 0 1em;
+  align-content: center;
+`
 
 export default function Todolist ({ todos, handleOnDelete, index }) {
   console.log('index', index)
@@ -11,12 +30,12 @@ export default function Todolist ({ todos, handleOnDelete, index }) {
     : <ul>
       {indexedMap(
           ({ todo }, idx) => (
-            <li key={idx} data-index={idx}>
+            <Li key={idx} data-index={idx}>
               {todo}
-              <button onClick={handleOnDelete.bind(index, null)}>
+              <Button onClick={handleOnDelete.bind(index, null)}>
                 &nbsp;X
-              </button>
-            </li>
+              </Button>
+            </Li>
           ),
           todos
         )}
